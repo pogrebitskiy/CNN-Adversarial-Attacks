e093d0e5ea82528b853b7b54a9c549f2f4dd9ffa
bzwyant <bzwyant@gmail.com>
2024-04-15 23:16:56 -0400

    presentation for website

---
+++ models/CIFAR10_Models.py
@@ -0,0 +1,96 @@
+import torch.nn as nn
+import torchvision.models as models
+
+
+class CIFAR10_FC500_100_10(nn.Module):
+    def __init__(self, num_classes=10):
+        super(CIFAR10_FC500_100_10, self).__init__()
+        self.fc1 = nn.Linear(3 * 32 * 32, 500)  # Adjust the input size to match CIFAR-10 images
+        self.fc2 = nn.Linear(500, 100)
+        self.fc3 = nn.Linear(100, num_classes)
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)  # Flatten the image
+        x = self.activation(self.fc1(x))
+        x = self.activation(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class CIFAR10_LeNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(CIFAR10_LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, kernel_size=5)
+        self.conv2 = nn.Conv2d(6, 16, kernel_size=5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, num_classes)
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+        x = self.activation(self.conv1(x))
+        x = nn.functional.max_pool2d(x, 2, 2)
+        x = self.activation(self.conv2(x))
+        x = nn.functional.max_pool2d(x, 2, 2)
+        x = x.view(-1, 16 * 5 * 5)
+        x = self.activation(self.fc1(x))
+        x = self.activation(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+class CIFAR10_VGG(nn.Module):
+    def __init__(self, num_classes=10):
+        super(CIFAR10_VGG, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        self.classifier = nn.Sequential(
+            nn.Linear(128 * 8 * 8, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+
+class CIFAR10_ResNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(CIFAR10_ResNet, self).__init__()
+        self.resnet = models.resnet18()
+        self.resnet.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, num_classes)
+
+    def forward(self, x):
+        x = self.resnet(x)
+        return x
+
+
+class CIFAR10_GoogLeNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(CIFAR10_GoogLeNet, self).__init__()
+        self.googlenet = models.googlenet(aux_logits=False)  # Disable auxiliary outputs
+        self.googlenet.conv1 = nn.Conv2d(3, 64, kernel_size=5, stride=1, padding=2, bias=False)  # Adjust for CIFAR10 image size
+        self.googlenet.fc = nn.Linear(self.googlenet.fc.in_features, num_classes)
+
+    def forward(self, x):
+        x = self.googlenet(x)
+        return x
